3b7a06dfe2ff564e2d421c655e14b5ad53e8a52c
seodongsu <xl445566@naver.com>
2022-01-28 16:24:51 +0900

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:react/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        "eslint:recommended",
-        "eslint-config-prettier",
-        "plugin:react/recommended",
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: "latest",
-        sourceType: "module"
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    plugins: [
-        "react",
-        "prettier",
-    ],
-    rules: {
-      "prettier/prettier": "error"
-    }
-}
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
