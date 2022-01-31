5e5440ba5c2bbe752d512653604cadeb2185236b
so098 <szerohan@gmail.com>
2022-01-31 11:48:56 +0900

    test push

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     node: true,
   },
   extends: [
-    //
     "eslint:recommended",
     "eslint-config-prettier",
     "plugin:react/recommended",
--- src/App.js
+++ src/App.js
@@ -3,34 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const dongsu = () => {
-    console.log("소영님 예민...");
-    console.log("넹....넹....넹...넹...");
-    console.log("ddddd");
-    console.log("에러를 확인함");
-    console.log("에러를 확인함");
-<<<<<<< HEAD
-    console.log("에러를 확인함");
-    console.log("as");
-    console.log("eslint");
-=======
->>>>>>> main
-  };
-
-  dongsu();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Edit <code>src/App.js</code> and save to reload.
-          <div>망쳐야징~~망쳐야징~~망쳐야징~~~</div>
-        </h1>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
